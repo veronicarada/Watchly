72cd219215d399796b6aa74299d6091be353302c
Josefina <finiaramburu1@gmail.com>
2026-05-14 12:42:56 -0300

    mejoras

--- frontend/src/views/Explore.vue
+++ frontend/src/views/Explore.vue
@@ -14,12 +14,18 @@
         <label>AÑO</label>
         <select v-model="filters.year" class="filter-select">
           <option value="">Todos los años</option>
-          <option value="2025">2025</option>
+           <option value="2026">2026</option>
+           <option value="2025">2025</option>
           <option value="2024">2024</option>
           <option value="2023">2023</option>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
+         <option value="2022">2022</option>
+         <option value="2021">2021</option>
+         <option value="2020">2020</option>
+         <option value="2019">2019</option>
+      <option value="2018">2018</option>
+    <option value="2017">2017</option>
+    <option value="2016">2016</option>
+    <option value="2015">2015</option>
         </select>
       </div>
       <div class="filter-group">
@@ -86,8 +92,8 @@ async function searchByQuery(q) {
 }
 
 onMounted(async () => {
-  const { data } = await api.genres()
-  genres.value = data?.genres || []
+  const genreData = await api.genres()
+genres.value = genreData?.genres || []
   if (route.query.q) searchByQuery(route.query.q)
   else applyFilters()
 })
